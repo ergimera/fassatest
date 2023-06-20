405d81de2eb58f31a273ee83db47d7788393bcef
Ergi <133374363+ergimera@users.noreply.github.com>
2023-06-20 12:37:39 +0200

    Update PDFdiff.py

--- PDFdiff.py
+++ PDFdiff.py
@@ -11,65 +11,65 @@ import docx
 st.title("PDF and Word Comparison")
 st.write("Select the PDF file and Word document to compare and save the differences.")
 
-# Choose the PDF file
-pdf_file = st.file_uploader("Select PDF File", type="pdf")
-if pdf_file is not None:
-    reader = PdfReader(pdf_file)
+# Choose the Word document
+word_file = st.file_uploader("Select Word Document", type="docx")
+if word_file is not None:
+    doc = docx.Document(word_file)
 
-    # Choose the Word document
-    word_file = st.file_uploader("Select Word Document", type="docx")
-    if word_file is not None:
-        doc = docx.Document(word_file)
+    # List to store extracted text from all pages
+    pages_text = []
 
-        # List to store extracted text from all pages
-        pages_text = []
+    # Iterating over all pages and extracting text from PDF
+    for page in range(len(reader.pages) - 1):
+        current_page = reader.pages[page]
+        text = current_page.extract_text()
+        pages_text.append(text)
 
-        # Iterating over all pages and extracting text from PDF
-        for page in range(len(reader.pages) - 1):
-            current_page = reader.pages[page]
-            text = current_page.extract_text()
-            pages_text.append(text)
+    all_text = "\n".join(pages_text)
 
-        all_text = "\n".join(pages_text)
+    # Get the text from the Word document
+    doc_text = '\n'.join([para.text for para in doc.paragraphs])
 
-        # Get the text from the Word document
-        doc_text = '\n'.join([para.text for para in doc.paragraphs])
+    # Split the texts into words
+    words1 = doc_text.split()
+    words2 = all_text.split()
 
-        # Split the texts into words
-        words1 = doc_text.split()
-        words2 = all_text.split()
+    # Get the differences between the words
+    differences = set(words2) - set(words1)
 
-        # Get the differences between the words
-        differences = set(words2) - set(words1)
+    # Print the differences
+    st.subheader("Differences:")
+    differing_words = list(differences)
 
-        # Print the differences
-        st.subheader("Differences:")
-        differing_words = list(differences)
+    for word in differing_words:
+        st.write(f'- {word}')
 
-        for word in differing_words:
-            st.write(f'- {word}')
+    # Save differing words in a Word file
+    output_file = st.text_input("Output file name", value="Differences.docx")
+    output_path = None
 
-        # Save differing words in a Word file
-        output_file = st.text_input("Output file name", value="Differences.docx")
-        output_path = None
+    if st.button("Save Differences"):
+        if output_file:
+            output_path = output_file.strip()
 
-        if st.button("Save Differences"):
-            if output_file:
-                output_path = output_file.strip()
+        if output_path:
+            # Create the Word document for output
+            output_doc = docx.Document()
+            output_doc.add_paragraph("Differences between PDF and Word document:")
 
+            # Create a bulleted list
+            list_paragraph = output_doc.add_paragraph()
+            list_paragraph.style = output_doc.styles["List Bullet"]
+
+            for word in differing_words:
+                list_paragraph.add_run(f"{word}\n")
+
+            output_path = st.text_input("Output file path", value="output/Differences.docx")
+            
             if output_path:
-                # Create the Word document for output
-                output_doc = docx.Document()
-                output_doc.add_paragraph("Differences between PDF and Word document:")
-
-                # Create a bulleted list
-                list_paragraph = output_doc.add_paragraph()
-                list_paragraph.style = output_doc.styles["List Bullet"]
-
-                for word in differing_words:
-                    list_paragraph.add_run(f"{word}\n")
-
                 output_doc.save(output_path)
                 st.success(f"Differences saved to: {output_path}")
             else:
-                st.error("Please enter a valid output file name.")
+                st.error("Please enter a valid output file path.")
+        else:
+            st.error("Please enter a valid output file name.")
